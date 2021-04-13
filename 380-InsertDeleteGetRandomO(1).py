2d0d87e7ea0cd8f6bcfe4982b359c4c5d93d3c11
Wenbo Shi <wenboshi@mit.edu>
2021-04-12 21:14:00 -0400

    04/12/2021

---
+++ 380-InsertDeleteGetRandomO(1).py
@@ -0,0 +1,42 @@
+from random import choice
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element = self.list[-1]
+            idx = self.dict[val]
+            self.list[idx] = last_element
+            self.dict[last_element] = idx
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)
